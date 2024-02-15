5c6aa3333db0577c0bd558930cc9705590d0421e
RomanKunal <0221csds107@niet.co.in>
2024-02-15 23:33:47 +0530

    updated

---
+++ Dom1.js
@@ -0,0 +1,46 @@
+/*
+
+
+
+
+Live reload enabled.
+favicon.ico:1 
+        
+        
+       Failed to load resource: the server responded with a status of 404 (Not Found)
+document.getElementById("title")
+<h1 id=​"title" class=​"heading">​…​</h1>​"DOM learning on Chai aur code "<span style=​"display:​ none;​">​test text​</span>​</h1>​
+const title=document.getElementById("title")
+undefined
+const a=document.getElementById("title")
+undefined
+a.style.backgroundColor="green"
+'green'
+a.innerText
+'DOM learning on Chai aur code'
+a.textContent
+'DOM learning on Chai aur code test text'
+document.querySelector(".heading")
+<h1 id=​"title" class=​"heading" style=​"background-color:​ green;​">​…​</h1>​
+document.querySelector("#title")
+<h1 id=​"title" class=​"heading" style=​"background-color:​ green;​">​…​</h1>​
+document.querySelector("input[type="password"]")
+VM1450:1 Uncaught SyntaxError: missing ) after argument list
+document.querySelector('input[type="password"]')
+<input type=​"password" name id aria-autocomplete=​"list">​
+const b= document.querySelector(".list-item")
+undefined
+b.style.backgroundColor="red"
+'red'
+const b= document.querySelectorAll(".list-item")
+undefined
+b.style.backgroundColor="red"
+VM1795:1 Uncaught TypeError: Cannot set properties of undefined (setting 'backgroundColor')
+    at <anonymous>:1:24
+(anonymous) @ VM1795:1
+const c= document.querySelectorAll(".list-item")
+undefined
+c.style.backgroundColor="yellow"
+
+
+*/
